8fd5d1070f0ac2655e694a84bfaaecc102b86da3
Ryczko <konradryczko1@gmail.com>
2021-03-24 13:52:06 +0100

    IP2-128: material UI added

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,18 @@
-import styled from 'styled-components'
+import styled from "styled-components";
+import Button from "@material-ui/core/Button";
 
 const Title = styled.h1`
-  font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
-`
+    font-size: 50px;
+    color: ${({ theme }) => theme.colors.primary};
+`;
 
 export default function Home() {
-  return <Title>Intivo Patronum</Title>
+    return (
+        <>
+            <Title>Intivo Patronum</Title>
+            <Button variant="contained" color="secondary">
+                przykładowy button z Material UI
+            </Button>
+        </>
+    );
 }
