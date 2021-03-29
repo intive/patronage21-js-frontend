0a062e740d09dda093224ee76935402a56504e04
Michał Ruta <ruciak1996@gmail.com>
2021-03-29 07:43:58 +0200

    IP2-137 return button and image

--- pages/error.js
+++ pages/error.js
@@ -1,53 +1,54 @@
 import styled from 'styled-components'
 import Button from '@material-ui/core/Button'
-import { atom, useRecoilState } from 'recoil'
+import { useRouter } from 'next/router'
+import Image from 'next/image'
 
 const ErrorTitle = styled.h1`
-    color: ${({ theme }) => theme.customPalette.colors.secondary};
+  color: ${({ theme }) => theme.customPalette.colors.secondary};
 `
-const ErrorDescripiton = styled.p`
-font-weight: bold;
-`
-const ErrorButton = styled(Button)`
-&&  {
+
+const StyledButton = styled(Button)`
+  && {
     font-weight: bold;
     width: 250px;
     height: 50px;
     border-radius: 50px;
     color: white;
-    font-size:1.2rem;
+    font-size: 1.2rem;
     text-transform: none;
-}
+  }
 `
 
-const errorTitleState = atom({
-  default: 'Wystąpił Błąd',
-  key: 'errorTitleState'
-})
-
-const errorDescriptionState = atom({
-  default: 'Wystąpił nieoczekiwany błąd',
-  key: 'errorDescriptionState'
-})
-
-const errorButtonTitleState = atom({
-  default: 'Strona główna',
-  key: 'errorButtonTitleState'
-})
-
-export default function ErrorPage () {
-  const title = useRecoilState(errorTitleState)
-  const buttonTitle = useRecoilState(errorButtonTitleState)
-  const description = useRecoilState(errorDescriptionState)
-
+export default function ErrorPage (props) {
+  const router = useRouter()
   return (
     <>
-      <ErrorTitle>{title}</ErrorTitle>
-      <ErrorDescripiton>{description}</ErrorDescripiton>
-      <ErrorButton variant='contained' color='primary'>
-        {buttonTitle}
-      </ErrorButton>
-      <ErrorButton variant='contained' color='secondary'>wróć</ErrorButton>
+      <ErrorTitle>{props.title}</ErrorTitle>
+      <p>{props.description}</p>
+      <StyledButton
+        variant='contained'
+        color='primary'
+        onClick={() => router.push(props.href)}
+      >
+        {props.buttonTitle}
+      </StyledButton>
+      {props.isReturn &&
+        <StyledButton
+          variant='contained'
+          color='secondary'
+          onClick={() => router.back()}
+        >
+          Wróć
+        </StyledButton>}
+      <Image src='/patronage-error.svg' alt='patronage' width={300} height={300} />
     </>
   )
+}
+
+ErrorPage.defaultProps = {
+  title: 'Wystąpił błąd',
+  description: 'Wystąpił nieoczekiwany błąd.',
+  buttonTitle: 'Strona główna',
+  href: '/',
+  isReturn: true
 }
