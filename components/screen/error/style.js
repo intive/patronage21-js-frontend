11e9649b4708ef212de14cca4edeb854011e42a4
Michał Ruta <ruciak1996@gmail.com>
2021-04-01 23:11:42 +0200

    file and code organize + small bugfix button
    -moved syles to separate file
    -moved component to screen section
    -some style changes
    -bugfix: button - font-size without size unit dosen't affect element

---
+++ components/screen/error/style.js
@@ -0,0 +1,21 @@
+import styled from 'styled-components'
+import Button from '../../elements/button'
+
+export const ErrorScreenWrapper = styled.div`
+display: flex;
+flex-direction: column;
+`
+
+export const ErrorTitle = styled.h1`
+  color: ${({ theme }) => theme.customPalette.colors.secondary};
+`
+
+export const StyledButton = styled(Button)`
+    width: 250px;
+    margin-top: 20px;
+  `
+export const StyledImage = styled.div`
+  display:flex;
+  margin-top: 20px;
+  justify-content: flex-end;
+`
