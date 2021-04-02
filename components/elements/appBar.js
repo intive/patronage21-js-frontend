9316844721902344eaab9e28c984a9a821af053e
joanna.figula@gmail.com <joanna.figula@gmail.com>
2021-04-02 15:06:22 +0200

    IP2-131 | move from jss to styled-component in header component

--- components/elements/appBar.js
+++ components/elements/appBar.js
@@ -1,147 +1,69 @@
 import React from 'react'
-import { fade, makeStyles } from '@material-ui/core/styles'
+import styled from 'styled-components'
 import AppBar from '@material-ui/core/AppBar'
 import Toolbar from '@material-ui/core/Toolbar'
 import IconButton from '@material-ui/core/IconButton'
-import InputBase from '@material-ui/core/InputBase'
 import PersonIcon from '@material-ui/icons/Person'
 import MenuIcon from '@material-ui/icons/Menu'
 import SearchIcon from '@material-ui/icons/Search'
 import Content from './content'
 import Logo from './logo'
+import { fade } from '@material-ui/core/styles'
+import SearchBar from './search'
 
-const useStyles = makeStyles((theme) => ({
-  grow: {
-    flexGrow: 1,
-    marginBottom: theme.spacing(3)
-  },
-  buttons: {
-    width: 'min-content',
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'flex-end',
-    '&  button': {
-      marginLeft: theme.spacing(2)
-    }
-  },
-  menuButton: {
-    marginRight: theme.spacing(0)
-  },
-  toolbar: {
-    padding: 0,
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'space-between'
-  },
-  title: {
-    display: 'none',
-    [theme.breakpoints.up('sm')]: {
-      display: 'block'
-    }
-  },
-  search: {
-    position: 'relative',
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'space-between',
-    border: `1px solid ${fade(theme.palette.text.primary, 0.2)}`,
-    backgroundColor: fade(theme.palette.common.white, 0.15),
-    '&:hover': {
-      backgroundColor: fade(theme.palette.common.white, 0.25)
-    },
-    marginRight: theme.spacing(2),
-    marginLeft: 0,
-    width: '100%',
-    [theme.breakpoints.up('sm')]: {
-      marginLeft: theme.spacing(3),
-      width: '35%'
-    }
-  },
-  searchIcon: {
-    padding: theme.spacing(0, 2),
-    height: '100%',
-    pointerEvents: 'none',
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
-    color: fade(theme.palette.text.primary, 0.5)
-  },
-  inputRoot: {
-    color: 'inherit',
-    width: '100%',
-    fontSize: '10px'
-  },
-  inputInput: {
-    padding: theme.spacing(1, 1, 1, 0),
-    // vertical padding + font size from searchIcon
-    paddingLeft: theme.spacing(1),
-    transition: theme.transitions.create('width'),
-    width: `calc(100% - ${theme.spacing(4)}px)`
+const Wrapper = styled.div`
+  ${({ theme }) =>`
+    flex-grow: 1;
+    margin-bottom: ${theme.spacing(3)}px
+  `}
+`
 
-  },
-  sectionDesktop: {
-    display: 'none',
-    [theme.breakpoints.up('md')]: {
-      display: 'flex'
-    }
-  },
-  sectionMobile: {
-    display: 'flex',
-    [theme.breakpoints.up('md')]: {
-      display: 'none'
-    }
-  },
-  icon: {
-    color: fade(theme.palette.text.primary, 0.5)
+const StyledToolbar = styled(Toolbar)`
+    padding: 0;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+`
+
+const Buttons = styled.div`
+          ${({ theme }) =>`
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+  button {
+    margin-left: ${theme.spacing(2)}px;
+    color: ${fade(theme.palette.text.primary, 0.5)}
   }
-}))
+  `}
+`
 
-export default function CustomAppBar ({ onChange }) {
-  const classes = useStyles()
 
+export default function CustomAppBar () {
   return (
-    <div className={classes.grow}>
+    <Wrapper>
       <AppBar color='transparent' position='static'>
         <Content>
-          <Toolbar className={classes.toolbar}>
+          <StyledToolbar>
             <Logo />
-            <div className={classes.search}>
-              <InputBase
-                placeholder='Wpisz czego szukasz'
-                onChange={onChange}
-                classes={{
-                  root: classes.inputRoot,
-                  input: classes.inputInput
-                }}
-                inputProps={{ 'aria-label': 'search' }}
-              />
-              <div className={classes.searchIcon}>
-                <SearchIcon />
-              </div>
-            </div>
-            <div className={classes.buttons}>
-              <IconButton
-                edge='start'
-                className={classes.menuButton}
-                color='inherit'
-                aria-label='open drawer'
-              >
-                <PersonIcon className={classes.icon} />
+
+            <SearchBar>
+              <SearchIcon />
+            </SearchBar>
+
+            <Buttons>
+              <IconButton>
+                <PersonIcon />
               </IconButton>
 
-              <IconButton
-                edge='start'
-                className={classes.menuButton}
-                color='inherit'
-                aria-label='open drawer'
-              >
-                <MenuIcon className={classes.icon} />
+              <IconButton>
+                <MenuIcon />
               </IconButton>
-            </div>
-          </Toolbar>
+            </Buttons>
+
+          </StyledToolbar>
         </Content>
-
       </AppBar>
-    </div>
+    </Wrapper>
   )
 }
