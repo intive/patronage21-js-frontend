1eaae547522660147fdf3a379b3b34746f30300a
Kbuczynski <kamil.buczynski00@interia.pl>
2021-05-08 12:09:24 +0200

    IP2-441: env configuration

--- next.config.js
+++ next.config.js
@@ -1,5 +1,18 @@
-module.exports = {
-  env: {
-    TEST_API_URL: 'http://localhost:8080/api/'
+const { PHASE_DEVELOPMENT_SERVER, PHASE_PRODUCTION_BUILD } = require('next/constants')
+
+module.exports = (phase) => {
+  const isDev = phase === PHASE_DEVELOPMENT_SERVER
+  const isProd = phase === PHASE_PRODUCTION_BUILD
+
+  const env = {
+    TEST_API_URL: (() => {
+      if (isDev) return 'http://localhost:8080/api/'
+      if (isProd) return 'https://api-patronage21.herokuapp.com/api/'
+      return 'http://localhost:8080/api/'
+    })()
+  }
+
+  return {
+    env
   }
 }
