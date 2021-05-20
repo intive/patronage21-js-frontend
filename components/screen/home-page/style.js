bd24d8adf44c1166974c16120750f00de173e1e5
weronika-p <78142146+weronika-p@users.noreply.github.com>
2021-05-20 16:15:06 +0200

    deleted flex-direction

--- components/screen/home-page/style.js
+++ components/screen/home-page/style.js
@@ -15,7 +15,6 @@ export const TilesContainer = styled.div`
     width: 100%;
     margin-top: 40px;
     display: flex;
-    flex-direction: row;
     justify-content: space-between;
     flex-wrap: wrap;
 `
